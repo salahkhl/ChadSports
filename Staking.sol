0ff3531f58fa0b5b8f962d9c44665d9cdf73b12c
salahkhl <khouiel.salah@gmail.com>
2022-11-10 00:45:33 +0100

    Publish Staking  Contract Updated

--- Staking.sol
+++ Staking.sol
@@ -5,35 +5,60 @@ pragma solidity ^0.8.0;
 import "./IERC1155.sol";
 import "./IERC1155Receiver.sol";
 import "./Ownable.sol";
+import "./ReentrancyGuard.sol";
 
+// Utils functions
 import "./Utils.sol";
 
-contract ChadStaking is Ownable {
+/// @title Chad Sports Staking.
+/// @author Memepanze
+/// @notice ERC1155 Staking contract for Chad Sports to rank the teams and get rewarded.
+contract ChadStaking is Ownable, ReentrancyGuard {
+
+    /// @notice ERC1155 interface variable
     IERC1155 public nft;
+
+    /// @notice map each address to a submissionId {uint} and inside of each: map a rank {uint} to a teamId {uint}
+    mapping (address => mapping(uint => mapping(uint => uint) )) public submission;
+
+    /// @notice 
+    mapping(address => uint) public submissionCount;
+
+    /// @notice
+    mapping(uint => mapping(address => uint)) public nftOwners;
+
+    /// @notice an array of the final ranking of the 4 teams in the World Cup
+    uint[4] public finalRanking;
+
+    /// @notice Check if the Final Ranking is set by contract owners after the World Cup if finished
+    bool public isFinalRankingSet;
+
+    /// @notice List of winners
+    address[] public winners;
+
+    // M O D I F I E R
+
+    /// @notice NFTs hodlers can change their rankings until 1 hour before the Top 16
+    modifier changeRankings {
+        require(block.timestamp <= 1670594400);
+        _;
+    }
 
     constructor(address _nft) {
         nft = IERC1155(_nft);
     }
 
-    // map each address to a submissionId(uint) and inside of each map a rank(uint) to a teamId(uint)
-    mapping (address => mapping(uint => mapping(uint => uint) )) public submission;
+    /// @notice User stake batch 4 teams (ERC1155) by ranking the NFTs from 1 to 4.
+    /// @dev This function can be called before the start of the Top 16 of the world cup.
+    function stakeBatch(uint8 _rank1, uint8 _rank2, uint8 _rank3, uint8 _rank4) external nonReentrant changeRankings {
 
-    mapping(address => uint[]) public submissionCount;
-
-    function getSubmissionLength(address _address) public view returns (uint){
-        return submissionCount[_address].length;
-    }
-
-    mapping(uint => mapping(address => uint)) public nftOwners;
-
-    function stakeBatch(uint8 _rank1, uint8 _rank2, uint8 _rank3, uint8 _rank4) public changeRankings {
-
-        uint subId = submissionCount[msg.sender].length;
+        // track of the number of submissions by address
+        uint subId = submissionCount[msg.sender];
         submission[msg.sender][subId][1] = _rank1;
         submission[msg.sender][subId][2] = _rank2;
         submission[msg.sender][subId][3] = _rank3;
         submission[msg.sender][subId][4] = _rank4;
-        submissionCount[msg.sender].push(1);
+        submissionCount[msg.sender]++;
 
         uint[] memory ids = new uint[](4);
         ids[0] = _rank1;
@@ -46,61 +71,18 @@ contract ChadStaking is Ownable {
         amounts[2] = 1;
         amounts[3] = 1;
 
+        // The ERC1155 are transfer to the contract address to avoid user to have multiple ranking with the same NFTs
         nft.safeBatchTransferFrom(msg.sender, address(this), ids, amounts, "");
 
+        // Track of the ownership of the NFTs which will be used to allow the user to unstake its staked ERC1155.
         nftOwners[_rank1][msg.sender]++;
         nftOwners[_rank2][msg.sender]++;
         nftOwners[_rank3][msg.sender]++;
         nftOwners[_rank4][msg.sender]++;
     }
 
-    function innerSwap(uint _submissionId, uint fromPosition, uint toPosition) public changeRankings {
-        // store the toPosition tokenId
-        uint _to = submission[msg.sender][_submissionId][toPosition];
-
-        // swap the "from" tokenId to "to" tokenId
-        submission[msg.sender][_submissionId][toPosition] = submission[msg.sender][_submissionId][fromPosition];
-        
-        // swap the "to" tokenId to "from" tokenId
-        submission[msg.sender][_submissionId][fromPosition] = _to;
-
-        //increase swaps number
-        swap[msg.sender][_submissionId]++ ;
-    }
-
-    function outerSwap(uint _submissionId, uint position, uint tokenId) public changeRankings {
-        uint previousTokenId = submission[msg.sender][_submissionId][position];
-        // replace the value in the mapping
-        submission[msg.sender][_submissionId][position] = tokenId;
-
-        // unstake the swapped tokenId
-        nft.safeTransferFrom(address(this), msg.sender, previousTokenId , 1, "");
-
-        // stake the new tokenId
-        nft.safeTransferFrom(msg.sender, address(this), tokenId, 1, "");
-
-        // add the msg.sender to the array of owners for the new tokenId
-        nftOwners[tokenId][msg.sender]++;
-
-        // remove the msg.sender from the array of owners for the previousTokenId
-        nftOwners[previousTokenId][msg.sender]--;
-
-        //increase swaps number
-        swap[msg.sender][_submissionId]++ ;
-    }
-
-    // Re-entrancy Guard
-
-    bool locked;
-
-    modifier reentrancyGuard() {
-        require(!locked);
-        locked = true;
-        _;
-        locked=false;
-    }
-
-    function unstakeBatch(uint256 _submissionId) public reentrancyGuard {
+    /// @notice User unstakes batch its 4 teams (ERC1155).
+    function unstakeBatch(uint256 _submissionId) external nonReentrant {
         require(nftOwners[submission[msg.sender][_submissionId][1]][msg.sender]>0, "#1 Not your NFT");
         require(nftOwners[submission[msg.sender][_submissionId][2]][msg.sender]>0, "#2 Not your NFT");
         require(nftOwners[submission[msg.sender][_submissionId][3]][msg.sender]>0, "#3 Not your NFT");
@@ -126,42 +108,20 @@ contract ChadStaking is Ownable {
         nft.safeBatchTransferFrom(address(this), msg.sender, ids, amounts, "");
     }
 
-    function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual returns (bytes4) {
-        return this.onERC1155Received.selector;
-    }
-
-    function onERC1155BatchReceived(address, address, uint256[] memory, uint256[] memory, bytes memory) public virtual returns (bytes4) {
-        return this.onERC1155BatchReceived.selector;
-    }
-
     // R E W A R D
 
-    uint[4] public finalRanking;
-    bool public isFinalRankingSet;
-
-    address[] public winnersCat1;
-    address[] public winnersCat2;
-    address[] public winnersCat3;
-
-    // for each address map a submissionId(uint) to a number of swaps(uint)
-    mapping (address => mapping(uint => uint )) public swap;
-
-    function isWinner(uint _submissionId) public {
+    /// @notice Check if a ranking submission is winning.
+    function isWinner(uint _submissionId) external {
         require(isFinalRankingSet, "Final Ranking Not Set");
         require(submission[msg.sender][_submissionId][1] == finalRanking[0], "Not eligible");
         require(submission[msg.sender][_submissionId][2] == finalRanking[1], "Not eligible");
         require(submission[msg.sender][_submissionId][3] == finalRanking[2], "Not eligible");
         require(submission[msg.sender][_submissionId][4] == finalRanking[3], "Not eligible");
         
-        if(swap[msg.sender][_submissionId] == 0){
-            winnersCat1.push(msg.sender);
-        } else if (swap[msg.sender][_submissionId] > 0 && swap[msg.sender][_submissionId] < 3){
-            winnersCat2.push(msg.sender);
-        } else {
-            winnersCat3.push(msg.sender);
-        }
+        winners.push(msg.sender);
     }
 
+    /// @notice Admin function to set the final ranking of the Top 4 for the World Cup.
     function setFinalRanking(uint _1, uint _2, uint _3, uint _4) public onlyOwner {
         finalRanking[0] = _1;
         finalRanking[1] = _2;
@@ -170,23 +130,20 @@ contract ChadStaking is Ownable {
         isFinalRankingSet = true;
     }
 
+    /// @notice Admin function to reward the list of Winners
     function rewardWinners() public payable onlyOwner {
-        for(uint i; i < winnersCat1.length; i++){
-            payable(address(winnersCat1[i])).transfer(address(this).balance*50/100*winnersCat1.length);
-        }
-        for(uint i; i < winnersCat2.length; i++){
-            payable(address(winnersCat2[i])).transfer(address(this).balance*30/100*winnersCat2.length);
-        }
-        for(uint i; i < winnersCat3.length; i++){
-            payable(address(winnersCat3[i])).transfer(address(this).balance*20/100*winnersCat3.length);
+        for(uint i; i < winners.length; i++){
+            payable(address(winners[i])).transfer(address(this).balance*50/100*winners.length);
         }
     }
 
-    // M O D I F I E R
 
-    modifier changeRankings {
-        require(block.timestamp <= 1670594400);
-        _;
+    function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(address, address, uint256[] memory, uint256[] memory, bytes memory) public virtual returns (bytes4) {
+        return this.onERC1155BatchReceived.selector;
     }
 
 }
